3ab8edb6c3ec90117e41379a024ed7788f3ac1a1
fahadAziz-byte <sp23-bse-107@cuilahore.edu.pk>
2024-12-17 17:23:31 +0500

    new commit

---
+++ Assignment-4/routes/admin/products.controller.js
@@ -0,0 +1,105 @@
+const express = require("express");
+let router = express.Router();
+router.use(express.urlencoded({ extended: true }));
+
+let Product = require("../../models/product.model");
+
+// Route to handle delete of a product
+router.get("/products/delete/:id", async (req, res) => {
+  try {
+    await Product.findByIdAndDelete(req.params.id);
+    return res.redirect("/admin/products");
+  } catch (error) {
+    console.error("Error deleting product:", error);
+    return res.status(500).send("Server Error");
+  }
+});
+
+// Route to render edit product form
+router.get("/products/edit/:id", async (req, res) => {
+  try {
+    let product = await Product.findById(req.params.id);
+    return res.render("admin/products/product-edit-form", {
+      layout: "adminlayout",
+      product,
+    });
+  } catch (error) {
+    console.error("Error fetching product:", error);
+    return res.status(500).send("Server Error");
+  }
+});
+
+router.post("/products/edit/:id", async (req, res) => {
+  try {
+    let product = await Product.findById(req.params.id);
+    product.title = req.body.title;
+    product.description = req.body.description;
+    product.price = req.body.price;
+    await product.save();
+    return res.redirect("/admin/products");
+  } catch (error) {
+    console.error("Error editing product:", error);
+    return res.status(500).send("Server Error");
+  }
+});
+
+// Route to display all products from MongoDB
+router.get("/products", async (req, res) => {
+  try {
+    let products = await Product.find(); 
+    return res.render("admin/products", {
+      layout: "adminlayout",
+      pageTitle: "Manage Your Products",
+      products,
+    });
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    return res.status(500).send("Server Error");
+  }
+});
+
+// Route to render the create product form
+router.get("/products/create", (req, res) => {
+  res.render('admin/products/create');
+});
+
+// Route to handle form submission and save product to MongoDB
+router.post("/products/create", async (req, res) => {
+  try {
+    
+    console.log(req.body)
+    let product = new Product(req.body);
+
+    await product.save(); 
+    return res.redirect("/admin/products");
+  } catch (error) {
+    console.error("Error saving product:");
+    res.status(500).send("Server Error");
+  }
+});
+
+
+router.get("/admin/products/:page?", async (req, res) => {
+  let page = req.params.page;
+  page = page ? Number(page) : 1;
+  let pageSize = 1;
+  let totalRecords = await Product.countDocuments();
+  let totalPages = Math.ceil(totalRecords / pageSize);
+  // return res.send({ page });
+  let products = await Product.find()
+    .limit(pageSize)
+    .skip((page - 1) * pageSize);
+
+  return res.render("admin/products", {
+    layout: "adminlayout",
+    pageTitle: "Manage Your Products",
+    products,
+    page,
+    pageSize,
+    totalPages,
+    totalRecords,
+  });
+});
+
+
+module.exports = router;
